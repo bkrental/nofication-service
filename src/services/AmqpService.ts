3c1cd24195f17e6ff6e577d029658db104572c18
Minh Phước <106443311+VideyKohaku@users.noreply.github.com>
2024-05-26 11:42:12 +0700

    Refactor/change express js to amqp ts (#4)
    * [Refactor] changed to ts, add amqp service
    
    * [Refactor] changed hardcode send email in amqp service into callback
    
    * [Fix] adding error check for invalid msg
    
    * [Style] remove console log for env variables
    
    * [Feat] change to 2 tier handle err message
    
    * Merge branch 'main' of github.com:bkrental/notification-service into refactor/change-express-js-to-amqp-ts
    
    * [Fix] fix template

--- src/services/AmqpService.ts
+++ src/services/AmqpService.ts
@@ -5,6 +5,7 @@ import getTemplate from "../utils/getTemplate";
 class AmqpService {
   static instance: AmqpService | null = null;
   channel: Channel;
+  retryCount: number = 0;
 
   constructor(channel: Channel) {
     this.channel = channel;
@@ -18,9 +19,42 @@ class AmqpService {
       }
       const connection = await amqplib.connect(rabbitmqUrl);
       const channel = await connection.createChannel();
-      await channel.assertQueue(queue, {
+
+      // declare exchange
+      const my_exchange = `my_exchange`;
+      await channel.assertExchange(my_exchange, "direct", { durable: true });
+
+      // declare queues
+      const email_queue = `email_queue`;
+      const delay_queue = `delay_queue`;
+      const dlx_queue = `dlx_queue`;
+
+      // assert queues
+      await channel.assertQueue(delay_queue, {
+        durable: true,
+        arguments: {
+          "x-dead-letter-exchange": my_exchange,
+          "x-dead-letter-routing-key": "",
+          "x-message-ttl": 5000,
+        },
+      });
+
+      await channel.assertQueue(email_queue, {
+        durable: true,
+        arguments: {
+          "x-dead-letter-exchange": my_exchange,
+          "x-dead-letter-routing-key": delay_queue,
+        },
+      });
+
+      await channel.assertQueue(dlx_queue, {
         durable: true,
       });
+
+      // bind queues to exchange
+      await channel.bindQueue(email_queue, my_exchange, "");
+      await channel.bindQueue(delay_queue, my_exchange, delay_queue);
+      await channel.bindQueue(dlx_queue, my_exchange, dlx_queue);
 
       return new AmqpService(channel);
     } catch (error) {
@@ -37,14 +71,46 @@ class AmqpService {
     return this.instance;
   }
 
-  public async startConsume(queueName: string, handleMsg: (msg: any) => void) {
+  public async startConsume(
+    queueName: string,
+    handleMsg: (msg: any) => Promise<void>
+  ) {
     try {
       if (!this.channel) {
         throw new Error("Channel is not initialized");
       }
-      await this.channel.consume(queueName, handleMsg, { noAck: true });
+      await this.channel.consume(
+        queueName,
+        async (msg: any) => {
+          try {
+            if (!msg) {
+              throw new Error("Message is empty");
+            }
+            console.log("startHandleMsg");
+            await handleMsg(msg);
+            this.channel.ack(msg);
+          } catch (error) {
+            console.error("Error consuming message");
+            console.error(error);
+            if (this.retryCount < 5) {
+              this.retryCount++;
+              return this.channel.nack(msg!, false, true);
+            } else {
+              this.retryCount = 0;
+              this.channel.ack(msg!);
+              const dlx_routing_key = "dlx_queue"; // specify the dead-letter routing key
+              this.channel.publish(
+                "my_exchange",
+                dlx_routing_key,
+                msg?.content as Buffer
+              );
+            }
+          }
+        },
+        { noAck: false }
+      );
     } catch (error) {
-      console.log("Error sending email");
+      console.error("Error starting to consume message");
       console.error(error);
     }
   }
